7939e2293ede0ef9956b967efb0c746008959fe1
Rafael Souza <159944889+rafasp97@users.noreply.github.com>
2024-04-22 18:49:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function click_menu(){
+    var menu = document.getElementById("menu_click");
+    if(menu.style.display === "block"){
+        menu.style.display = "none"
+    }
+    else{
+        menu.style.display = "block"
+    }
+}
