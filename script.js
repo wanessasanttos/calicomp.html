d3d78eb6b681fadc2fcdcde647469652324a7901
Rafael Souza <159944889+rafasp97@users.noreply.github.com>
2024-04-24 20:26:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//MENU MOBILE -------------------------------------------
 function click_menu(){
     var menu = document.getElementById("menu_click");
     if(menu.style.display === "block"){
@@ -6,4 +8,21 @@ function click_menu(){
     else{
         menu.style.display = "block"
     }
+}
+
+//SLIDESHOW NOTÍCIAS -------------------------------------
+let contador = 1;
+
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    proximaImagem()
+}, 4000)
+
+function proximaImagem(){
+    contador++;
+    if(contador > 5){
+        contador = 1;
+    }
+    document.getElementById("radio"+contador).checked = true;
 }
