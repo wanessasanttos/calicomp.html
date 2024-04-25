0f92039d0b9aecdadd6e243c959cb094cc9c4c69
Rafael Souza <159944889+rafasp97@users.noreply.github.com>
2024-04-25 15:12:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,12 +17,19 @@ document.getElementById("radio1").checked = true;
 
 setInterval(function(){
     proximaImagem()
-}, 4000)
+}, 7000)
 
 function proximaImagem(){
     contador++;
     if(contador > 5){
         contador = 1;
+    }
+    document.getElementById("radio"+contador).checked = true;
+}
+function antecederImagem(){
+    contador--;
+    if(contador < 1){
+        contador = 5;
     }
     document.getElementById("radio"+contador).checked = true;
 }
