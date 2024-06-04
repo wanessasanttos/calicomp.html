7940a1d1e8dc9c5a3bb0996bd30f88e34f6c5465
Rafael Souza <159944889+rafasp97@users.noreply.github.com>
2024-06-03 21:50:13 -0300

    correção do carrossel
    mudança no script para q o carrossel funcione no vercel

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function click_menu(){
 }
 
 //SLIDESHOW NOTÍCIAS -------------------------------------
-if(window.location.pathname.includes('/index.html') || window.location.hostname.includes('/calicomp.vercel.app')){ //verifica se é a página inicial
+if(window.location.pathname.includes('/index.html') || window.location.hostname.includes('calicomp.vercel.app')){ //verifica se é a página inicial
     let contador = 1;
 
     document.getElementById("radio1").checked = true;
